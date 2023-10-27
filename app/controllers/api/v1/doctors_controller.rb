35bb54e861beb69241b5d5b07c155012178d6ac1
akezeth <akezeth@gmail.com>
2023-10-27 14:33:05 +0300

    Add Doctors controller with required actions

---
+++ app/controllers/api/v1/doctors_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all.includes(:appointments)
+    render json: @doctors
+  end
+
+  def show
+    @doctors = Doctor.all
+    @doctor = @doctors.find(params[:id])
+    render json: @doctor
+  end
+
+  def create
+    @specialization = Specialization.find(params[:specialization_id])
+    @doctor = @specialization.doctors.new(doctor_params)
+
+    if @doctor.save
+      render json: @doctor, status: :created
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+      render json: @doctor, status: :success
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @doctor.destroy
+      render json: { message: 'Doctor was successfully destroyed' }, status: :ok
+    else
+      render json: { errors: @doctor.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:doc_name, :profile_picture, :bio, :time_available_from,
+                                   :time_available_to).merge(specialization_id: @specialization.id)
+  end
+end
