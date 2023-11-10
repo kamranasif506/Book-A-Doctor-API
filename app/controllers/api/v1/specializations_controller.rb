54609a8291c4b7609a41534d1523e45c010abdbb
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-11-10 15:22:06 +0500

    Function to insert Specialization

--- app/controllers/api/v1/specializations_controller.rb
+++ app/controllers/api/v1/specializations_controller.rb
@@ -3,4 +3,17 @@ class Api::V1::SpecializationsController < ApplicationController
     @specializations = Specialization.all
     render json: @specializations
   end
+  def create
+    @specialization = Specialization.new(specialization_params);
+    if @specialization.save
+      render json: @specialization, status: :created
+    else
+      render json: @specialization.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def specialization_params
+    params.require(:specialization).permit(:name)
+  end
 end
